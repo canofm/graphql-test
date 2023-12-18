46f874b3260c54300fcde1057c6297470fe1a9d6
Federico Cano <canofedericomartin@gmail.com>
2023-12-18 19:57:05 -0300

    Add uuid extensions to postgres

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -11,4 +11,8 @@ function pg_run_command() {
 
 pg_run_command "SELECT 'CREATE DATABASE fede_subscriptions_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'fede_subscriptions_db')"
 
+pg_run_command 'CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";' 'fede_subscriptions_db'
+
 pg_run_command "SELECT 'CREATE DATABASE fede_subscriptions_db_test' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'fede_subscriptions_db_test')"
+
+pg_run_command 'CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";' 'fede_subscriptions_db_test'
