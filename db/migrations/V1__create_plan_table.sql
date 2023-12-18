9ef6a308e1936180e1e35863793ded1ed8a6ddfb
Federico Cano <canofedericomartin@gmail.com>
2023-12-18 19:57:31 -0300

    Add create plans table migration

---
+++ db/migrations/V1__create_plan_table.sql
@@ -0,0 +1,18 @@
+CREATE TYPE currency_enum AS ENUM('usd', 'peso argentino');
+CREATE TYPE billing_frequency_enum AS ENUM('monthly', 'annual');
+CREATE TYPE storage_unit_enum AS ENUM('GB', 'TB');
+CREATE TYPE support_level_enum AS ENUM('basic', 'priority', 'dedicated');
+
+CREATE TABLE plans(
+  "id" UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  "name" VARCHAR NOT NULL,
+  "description" TEXT,
+  "monthly_price" DECIMAL NOT NULL,
+  "currency" currency_enum NOT NULL,
+  "billing_frequency" billing_frequency_enum NOT NULL,
+  "storage_amount" integer,
+  "storage_unit" storage_unit_enum,
+  "support_level" support_level_enum NOT NULL,
+  "customizations" JSONB NOT NULL DEFAULT '{}',
+  "annual_discount" DECIMAL
+)
