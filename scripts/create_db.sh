eec184a13355c6ddae8b456a5c6fcdcb7fc27aa7
Federico Cano <canofedericomartin@gmail.com>
2023-12-18 19:11:20 -0300

    Update db name

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -9,6 +9,6 @@ function pg_run_command() {
     docker exec fede_postgres /bin/sh -c "PGPASSWORD=admin echo \"$1\\gexec\" | psql -U admin -d $db_name"
 }
 
-pg_run_command "SELECT 'CREATE DATABASE graphql_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'graphql_db')"
+pg_run_command "SELECT 'CREATE DATABASE fede_subscriptions_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'fede_subscriptions_db')"
 
-pg_run_command "SELECT 'CREATE DATABASE graphql_db_test' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'graphql_db_test')"
+pg_run_command "SELECT 'CREATE DATABASE fede_subscriptions_db_test' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'fede_subscriptions_db_test')"
