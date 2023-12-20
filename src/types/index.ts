ed18374459f48e05e06425021c2a68ecaf70a5eb
Federico Cano <canofedericomartin@gmail.com>
2023-12-20 08:32:20 -0300

    Add Plan types

--- src/types/index.ts
+++ src/types/index.ts
@@ -18,3 +18,45 @@ export enum SupportLevel {
   Dedicated = 'dedicated',
   Priority = 'priority',
 }
+
+export type Customizations = {
+  advancedAnalytics?: boolean | null;
+  analyticsTools?: boolean | null;
+  customIntegrations?: boolean | null;
+};
+
+export type Plan = {
+  id: string;
+  name: string;
+  description: string | null;
+  monthlyPrice: number;
+  currency: Currency;
+  billingFrequency: BillingFrequency;
+  storageAmount: number;
+  storageUnit: StorageUnit;
+  supportLevel: SupportLevel;
+  customizations: Customizations;
+  annualDiscount: number;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+};
+
+export const PLAN_FIELDS = <const>[
+  'id',
+  'name',
+  'description',
+  'monthlyPrice',
+  'currency',
+  'billingFrequency',
+  'storageAmount',
+  'storageUnit',
+  'supportLevel',
+  'customizations',
+  'annualDiscount',
+  'createdAt',
+  'updatedAt',
+  'deletedAt',
+];
+
+export type PlanField = keyof Plan;
