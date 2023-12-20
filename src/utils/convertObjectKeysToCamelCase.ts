39a03a02451836c0394b2b89488241f0ac8fa558
Federico Cano <canofedericomartin@gmail.com>
2023-12-20 02:20:41 -0300

    Fix type

--- src/utils/convertObjectKeysToCamelCase.ts
+++ src/utils/convertObjectKeysToCamelCase.ts
@@ -1,5 +1,7 @@
 import { camelCase, mapKeys } from 'lodash';
 
-export default function convertObjectKeysToCamelCase<T extends Record<string, unknown>>(o: T): T {
+export default function convertObjectKeysToCamelCase<T extends Record<string, unknown>>(
+  o: Record<string, unknown>,
+): T {
   return mapKeys(o, (v: unknown, k: string) => camelCase(k)) as unknown as T;
 }
