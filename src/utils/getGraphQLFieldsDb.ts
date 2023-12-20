b373a43d2bab0886ef2b34b516c157635f0747f3
Federico Cano <canofedericomartin@gmail.com>
2023-12-20 08:33:40 -0300

    WIP refactor get graphql fields

--- src/utils/getGraphQLFieldsDb.ts
+++ src/utils/getGraphQLFieldsDb.ts
@@ -5,3 +5,7 @@ import { snakeCase } from 'lodash';
 export function getGraphQLFieldsDb<T>(params: GraphQLResolveInfo): T[] {
   return Object.keys(graphqlFields(params)).map(snakeCase) as unknown as T[];
 }
+
+export function getGraphQLFields<T>(params: GraphQLResolveInfo): T[] {
+  return Object.keys(graphqlFields(params)) as unknown as T[];
+}
