d66d59e5de0b3071673cc793319c0293f795a510
Federico Cano <canofedericomartin@gmail.com>
2023-12-20 01:37:27 -0300

    Add commons types

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export enum Currency {
+  USD = 'usd',
+  PesoArgentino = 'peso_argentino',
+}
+
+export enum BillingFrequency {
+  Annual = 'annual',
+  Monthly = 'monthly',
+}
+
+export enum StorageUnit {
+  GB = 'GB',
+  TB = 'TB',
+}
+
+export enum SupportLevel {
+  Basic = 'basic',
+  Dedicated = 'dedicated',
+  Priority = 'priority',
+}
